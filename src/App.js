6281a964ffa214650c29066cad181f74ff79d618
SAMUEL ALEJANDRO HURTADO JIMENEZ <samuelhurtado20@hotmail.com>
2022-04-06 22:06:21 -0500

    add form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 //import logo from './logo.svg';
 //import './App.css';
 
+import Form from "./components/Form";
+
 function App() {
   return (
     <div className="App">
       
-    
+    <Form />
+
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,38 @@
-import React from 'react'
+//import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 export default function Form() {
-  return (
-    <div>Form</div>
-  )
+    const [name, setName] = useState('')
+    const [lastName, setlastName] = useState('')
+    const [salary, setSalary] = useState('')
+
+    const addEmployee = async (e) => {
+        e.preventDefault()
+        const newEmployee = { name, lastName, salary }
+        const response = await axios.post('http://localhost:4020/api/test/create', newEmployee)
+        console.log(response)
+    }
+
+    return (
+        <div className="container col-md-4 mt-4">
+            <form onSubmit={addEmployee}>
+                <div className="mb-3">
+                    <label for="name" className="form-label">Name</label>
+                    <input type="text" className="form-control" onChange={e => setName(e.target.value)} placeholder='Name' required />
+                </div>
+
+                <div className="mb-3">
+                    <label for="lasName" className="form-label">Last name</label>
+                    <input type="text" className="form-control" onChange={e => setlastName(e.target.value)} placeholder='Last Name' required />
+                </div>
+
+                <div className="mb-3">
+                    <label for="salary" className="form-label">Salary</label>
+                    <input type="number" className="form-control" onChange={e => setSalary(e.target.value)} placeholder='salary' required />
+                </div>
+                <button type="submit" className="btn btn-primary">Save</button>
+            </form>
+        </div>
+    )
 }
